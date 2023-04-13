8846f55f304765b5960bfcc226ee0911136ad606
Ujjwal Sarswat <ujjwalsarswat1@gmail.com>
2023-04-13 23:02:48 +0530

    add(*): add even more data

--- download.py
+++ download.py
@@ -7,8 +7,8 @@ api.authenticate()
 # kg.api.authenticate()
 # kg.api.dataset_download_files('g2net-gravitational-wave-detection/train/0/0/0/00000e74ad.npy', path='./data', unzip=True)
 dwnfiles = np.loadtxt('training_labels.csv', dtype=str, delimiter=',', usecols=(0))
-dwnfiles = dwnfiles[:263]
+dwnfiles = dwnfiles[157:263]
 
 for dwnfile in dwnfiles:
-    print()
+    # print('train/0/0/'+dwnfile[2]+'/'+dwnfile+'.npy')
     api.competition_download_file('g2net-gravitational-wave-detection','train/0/0/'+dwnfile[2]+'/'+dwnfile+'.npy', path='./data')
